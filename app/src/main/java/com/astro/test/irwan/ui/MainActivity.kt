e5420ceaa4fa70a147d46255dcb3a670967f7f13
Irwan Hermawan <rwnhermawan@gmail.com>
2022-10-18 10:27:41 +0700

    Initial commit

---
+++ app/src/main/java/com/astro/test/irwan/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.astro.test.irwan.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.astro.test.irwan.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
